92c3e7fdad6c081492f5ce6187a7d55100c8718b
Szymon Lesisz <sz.lesisz@gmail.com>
2019-12-12 13:33:54 +0100

    Update UDPBridge.java

--- android/app/src/main/java/com/rnbridge/bridge/UDPBridge.java
+++ android/app/src/main/java/com/rnbridge/bridge/UDPBridge.java
@@ -1,7 +1,6 @@
 package com.rnbridge.bridge;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.rnbridge.TrezorException;
@@ -22,19 +21,22 @@ import java.util.List;
 
 public class UDPBridge implements BridgeInterface {
     private static final String TAG = UDPBridge.class.getSimpleName();
+    private static final String EMULATOR_UDP_HOST = "10.0.2.2";
     private static final int EMULATOR_UDP_PORT = 21324;
+    private final byte[] PING = "PINGPING".getBytes();
+    private final String PONG = "PONGPONG";
     private static UDPBridge instance;
 
     private Context context;
 
-    private List<TrezorInterface> trezorDeviceList;
+    private List<TrezorInterface> trezorDeviceList = new ArrayList<>();
 
     public UDPBridge(Context context){
         this.context = context;
     }
 
     public static UDPBridge getInstance(Context context){
-        if (instance==null){
+        if (instance == null){
             instance = new UDPBridge(context);
         }
         return instance;
@@ -42,25 +44,22 @@ public class UDPBridge implements BridgeInterface {
 
     @Override
     public List<TrezorInterface> enumerate() {
-        if (trezorDeviceList==null || trezorDeviceList.size()==0) {
-            trezorDeviceList = new ArrayList<>();
+        if (trezorDeviceList.size() == 0) {
             if (checkDevice()){
-                trezorDeviceList.add(new TrezorDevice("emulator"));
-                return trezorDeviceList;
-            }else{
-                return trezorDeviceList;
+                trezorDeviceList.add(new TrezorDevice("udp-emulator"));
             }
-        } else{
+        } else {
             if (!checkDevice()) {
                 trezorDeviceList = new ArrayList<>();
             }
-            return trezorDeviceList;
         }
+
+        return trezorDeviceList;
     }
 
     @Override
     public TrezorInterface getDeviceByPath(String path) {
-        if (trezorDeviceList.size()>0){
+        if (trezorDeviceList.size() > 0){
             return trezorDeviceList.get(0);
         }else{
             return enumerate().get(0);
@@ -72,19 +71,20 @@ public class UDPBridge implements BridgeInterface {
     }
 
     private boolean checkDevice(){
-        byte[] firstMessage = Utils.hexStringToByteArray("2323000000000000");
-        String firstMessageResponse = "23230011000000A88802000A097472657A6F722E696F1002180120093218344130463046394232313336383439413043443945463042380040004A07656E676C69736852047465737490020060016A0E64303839376162635F6469727479800100880100980100A00100AA010154D80100E00100E80100F00101F00102F00103F00104F00105F00106F00107F00108F00109F0010AF00111F0010BF0010CF0010DF0010EF0010FF00110F8010080020100000000000000000000000000";
-        byte[] secondMessage = Utils.hexStringToByteArray("23230001000000020a00");
-        String secondMessageResponse = "23230002000000020A000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-        TrezorInterface trezorDevice = new TrezorDevice("emulator");
         try {
-            trezorDevice.openConnection(context);
-            trezorDevice.rawPost(firstMessage);
-            byte[] b = trezorDevice.rawRead();
-            trezorDevice.rawPost(secondMessage);
-            byte[] b2 = trezorDevice.rawRead();
-            return firstMessageResponse.equals(Utils.byteArrayToHexString(b)) && secondMessageResponse.equals(Utils.byteArrayToHexString(b2));
-        }catch (TrezorException e){
+            InetAddress socketAddress = InetAddress.getByName(EMULATOR_UDP_HOST);
+            DatagramSocket socket = new DatagramSocket();
+            socket.setSoTimeout(500);
+            DatagramPacket pingPacket = new DatagramPacket(PING, PING.length, socketAddress, EMULATOR_UDP_PORT);
+            socket.send(pingPacket);
+
+            byte[] pong = new byte[8];
+            DatagramPacket pongPacket = new DatagramPacket(pong, pong.length, socketAddress, EMULATOR_UDP_PORT);
+            socket.receive(pongPacket);
+            socket.close();
+
+            return new String(pong).equals(PONG);
+        } catch (IOException e) {
             return false;
         }
     }
@@ -142,7 +142,8 @@ public class UDPBridge implements BridgeInterface {
                 try {
                     socket.receive(datagramPacketIn);
                 } catch (IOException e) {
-                    throw new TrezorException("Socket IOException",e);
+                    Log.e(TAG, "messageRead: read from socket failed");
+                    e.printStackTrace();
                 }
                 Log.i(TAG, String.format("messageRead: Read chunk: %d bytes", b.length));
 
@@ -172,7 +173,7 @@ public class UDPBridge implements BridgeInterface {
                 try {
                     socket.receive(datagramPacketIn);
                 } catch (IOException e) {
-                    throw new TrezorException("Socket IOException",e);
+                    e.printStackTrace();
                 }
                 Log.i(TAG, String.format("messageRead: Read chunk (cont): %d bytes", b.length));
                 if (b[0] != (byte) '?') {
@@ -192,10 +193,9 @@ public class UDPBridge implements BridgeInterface {
         @Override
         public void openConnection(Context context) throws TrezorException {
             try {
-                socketAddress = InetAddress.getByName("10.0.2.2");
+                socketAddress = InetAddress.getByName(EMULATOR_UDP_HOST);
                 if (socket == null) {
-                    socket = new DatagramSocket();
-                    socket.setSoTimeout(500);
+                    socket = new DatagramSocket(EMULATOR_UDP_PORT);
                 }
             } catch (SocketException e) {
                 e.printStackTrace();
@@ -212,7 +212,6 @@ public class UDPBridge implements BridgeInterface {
             socket = null;
         }
 
-        @NonNull
         @Override
         public String toString() {
             return "{\"path\":\"" + this.path + "\",\"session\": null}";
